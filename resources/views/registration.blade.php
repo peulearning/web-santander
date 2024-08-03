b3bb922e8f746c8697eaa3c21c4f8fb6dcdd2a20
Pedro <peuhenry2018@gmail.com>
2024-08-03 19:52:16 -0300

    Initial Commit ✨

--- resources/views/registration.blade.php
+++ resources/views/registration.blade.php
@@ -21,7 +21,7 @@
 
     </head>
     <body>
-        <a href="/" class="fixed-top pull-left m-2"><button type="button" class="btn" id="btn-back">
+        <a href="/Login" class="fixed-top pull-left m-2"><button type="button" class="btn" id="btn-back">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left" viewBox="0 0 16 16">
             <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8"></path>
             </svg>
@@ -68,13 +68,13 @@
                         </ul>
                     </div>
                     <div class="d-flex justify-content-center">
-                    <input type="submit" value="Acessar" class="btn w-50" id="btn-reg">
+                    <input type="submit" value="Cadastrar" class="btn w-50" id="btn-reg">
                     </div>
                 </form>
             </section>
             <section class="d-flex justify-content-center mt-3" id="msg-not-registration">
                 <p>Caso já tenha acesso, <a href="/Login" id="btn-registration">ENTRAR</a></p>
-            </section> 
+            </section>
         </section>
     </body>
 </html>
